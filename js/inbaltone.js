4d0cdcb3ec5fc1bbd61aec23ffc7077c2ed10871
Avnerus <avnerus@gmail.com>
2014-07-11 02:10:29 +0300

    playing sounds

--- js/inbaltone.js
+++ js/inbaltone.js
@@ -2,61 +2,6 @@ var Vue = require('vue');
 var vm = new Vue({
 	el: '.main-content',
 	components: {
-		'my-wizard': {
-			data: {
-				item: {
-					firstName: 'John',
-					lastName: 'Doe',
-					age: 22
-				},
-				ui: {}
-			},
-			replace: true,
-			template: '#my-wizard',
-			created: function () {
-				this.ui = {
-					count: $('div.page', this.$compiler.rawContent).length,
-					width: $(this.$el).width(),
-					pages: [],
-					index: 0
-				};
-				for (var i = 0; i < this.ui.count; i++) {
-					this.ui.pages.push(i);
-				}
-			},
-			computed: {
-				canvasWidth: function () {
-					return (this.ui.count * this.ui.width + 20) + 'px';
-				},
-				translateX: function(){
-					return 'translateX(-' + (this.ui.width * this.ui.index) + 'px)';
-				}
-			},
-			methods: {
-				moveTo: function (idx) {
-					this.ui.index = idx;
-				},
-				putPageContents: function(idx){
-					var src = $('div.page', this.$compiler.rawContent).eq(idx).html();
-					return this.$compiler.compile(src, true);
-
-				},
-				prevPage: function(){
-					if(this.ui.index > 0){
-						this.ui.index--;
-					}
-				},
-				nextPage: function(){
-					if(this.ui.index < this.ui.count){
-						this.ui.index++;
-					}
-				},
-				finish: function(){
-					window.alert('finished! your data: ' + JSON.stringify(this.item) );
-				}
-			}
-
-
-		}
+        'my-wizard': require('./mywizard')
 	}
 });
